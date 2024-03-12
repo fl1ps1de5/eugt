dc1453f5129274262fd5a9bbb7f63b510c6c05f7
fl1ps1de5 <ethanhirschowitz@protonmail.com>
2024-03-12 16:30:58 +1100

    New files + struc

--- ES/policies.py
+++ ES/policies.py
@@ -49,3 +49,16 @@ class MLP(nn.Module):
 
     def get_weights(self):
         return  nn.utils.parameters_to_vector(self.parameters()).detach().numpy()
+    
+class simpleMLP(nn.Module):
+    def __init(self, state_dim, action_dim):
+        super(simpleMLP, self).__init__()
+        self.linear1 = nn.Linear(state_dim, 64)
+        self.linear2 = nn.Linear(64, 32)
+        self.out = nn.Linear(32, action_dim)
+
+    def forward(self, x):
+        x = nn.functional.relu(self.linear1(x))
+        x = nn.functional.relu(self.linear2(x))
+        o = self.out(x)
+        return o.squeeze()
